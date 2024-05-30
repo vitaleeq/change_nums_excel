5d357395e5267238149360b8d40c083a358c389e
vitaleeq <vitali.tsarev@gmail.com>
2024-05-30 09:27:02 +0300

    commit number ?

--- functional_tools.py
+++ functional_tools.py
@@ -2,18 +2,22 @@ from random import choice
 from string import ascii_uppercase as letters
 
 
-def create_new_filename(file_name):
-    return file_name if '/' not in file_name else file_name[file_name.rfind('/')+1:]
+def create_new_filename(filename):
+    return filename if '/' not in filename else filename[filename.rfind('/')+1:]
 
 
 def random_last_symbols(serial_number):
-    new_serial_number = serial_number[:-3]
-    for char in serial_number[-3:]:
-        if char.isnumeric():
-            new_serial_number += choice('0123456789'.replace(char, ''))
-        else:
-            new_serial_number += choice(letters.replace(char, ''))
-    return new_serial_number
+    quantity_symbols = -3
+    if validate_string(serial_number, quantity_symbols):
+        new_serial_number = serial_number[:quantity_symbols]      # -2 => -3
+        for char in serial_number[quantity_symbols:]:             # -2 => -3
+            if char.isnumeric():
+                new_serial_number += choice('0123456789'.replace(char, ''))
+            else:
+                new_serial_number += choice(letters.replace(char, ''))
+        return new_serial_number
+    else:
+        return serial_number
 
 
 def create_cells_dictionary(wb_sheet, cell_names):
@@ -22,3 +26,7 @@ def create_cells_dictionary(wb_sheet, cell_names):
     cells_with_serial_numbers = {elem[0].coordinate: str(elem[1]).split(', ') for elem in ((cell, cell.value) for col \
         in wb_sheet.iter_cols(min_col=column, max_col=column, min_row=start_row, max_row=finish_row) for cell in col)}
     return cells_with_serial_numbers
+
+
+def validate_string(string_vc, quantity_symbols):
+    return len(string_vc) < quantity_symbols
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from openpyxl import load_workbook
 import functional_tools as ft
 
 
+# 'Спецификация FC Advant upd.xlsx'     E10:E44
+# 'Спецификация Tissot Daghaya (2).xlsx'        F10, F51
 if __name__ == "__main__":
     filename = input('Input the full filepath: ')     # /home/vitaly/Downloads/test.xlsx
     sheet_name = input('Input sheet name: ')
@@ -10,7 +12,6 @@ if __name__ == "__main__":
     # load file, activate sheet in it, select targeted cell's
     wb = load_workbook(filename=filename)
     wb_sheet = wb.active if sheet_name == '' else wb[sheet_name]
-    cell_range = wb_sheet['Q10:Q124']
 
     # create dictionary with keys==cell's coords, values==cell's values
     cells_with_serial_numbers = ft.create_cells_dictionary(wb_sheet, cell_names)
