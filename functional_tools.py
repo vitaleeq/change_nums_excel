502d46cba70bdca5df0b8aef30fc60cb213f539f
vitaleeq <vitali.tsarev@gmail.com>
2024-09-13 13:22:22 +0300

    new version with opportunity to select which symbols to change

--- functional_tools.py
+++ functional_tools.py
@@ -6,18 +6,36 @@ def create_new_filename(filename):
     return filename if '/' not in filename else filename[filename.rfind('/')+1:]
 
 
-def random_last_symbols(serial_number):
-    quantity_symbols = -3
-    if validate_string(serial_number, quantity_symbols):
-        new_serial_number = serial_number[:quantity_symbols]      # -2 => -3
-        for char in serial_number[quantity_symbols:]:             # -2 => -3
+def random_symbols(serial_number, first_symbols=0, last_symbols=0):
+    #quantity_last_symbols = 0  # Сколько конечных символов нужно поменять
+    #quantity_first_symbols = 3  # Сколько начальных символов нужно поменять
+    new_serial_number = serial_number
+
+    # for change last symbols __________________________________________
+    if last_symbols:
+        temp_serial_number = ""
+        for char in serial_number[-last_symbols:]:
             if char.isnumeric():
-                new_serial_number += choice('0123456789'.replace(char, ''))
+                temp_serial_number += choice('0123456789'.replace(char, ''))
             else:
-                new_serial_number += choice(letters.replace(char, ''))
-        return new_serial_number
-    else:
-        return serial_number
+                temp_serial_number += choice(letters.replace(char, ''))
+        new_serial_number = new_serial_number[:-last_symbols] + temp_serial_number
+    # __________________________________________________________________
+
+    # for change first symbols__________________________________________
+    if first_symbols:
+        temp_serial_number = ""
+        for char in serial_number[:first_symbols]:  # -2 => -3
+            if char.isnumeric():
+                temp_serial_number += choice('0123456789'.replace(char, ''))
+            else:
+                temp_serial_number += choice(letters.replace(char, ''))
+        new_serial_number = temp_serial_number + new_serial_number[first_symbols:]
+    # __________________________________________________________________
+
+    return new_serial_number
+    #else:
+        #return serial_number
 
 
 def create_cells_dictionary(wb_sheet, cell_names):
@@ -29,4 +47,4 @@ def create_cells_dictionary(wb_sheet, cell_names):
 
 
 def validate_string(string_vc, quantity_symbols):
-    return len(string_vc) < quantity_symbols
+    return len(string_vc) < abs(quantity_symbols)
--- main.py
+++ main.py
@@ -2,12 +2,11 @@ from openpyxl import load_workbook
 import functional_tools as ft
 
 
-# 'Спецификация FC Advant upd.xlsx'     E10:E44
-# 'Спецификация Tissot Daghaya (2).xlsx'        F10, F51
+# 'Спецификация Tissot Kelechek 09 24 16TIS-0524KL.xlsx'        E10:E85
 if __name__ == "__main__":
-    filename = input('Input the full filepath: ')     # /home/vitaly/Downloads/test.xlsx
-    sheet_name = input('Input sheet name: ')
-    cell_names = list(map(str.strip, input('Input start_cell and finish_cell names, e.g. A1, A10: ').split(',')))       # Q10:Q124
+    filename = input('Input the full filepath (allow only filename, if file is in a program root-directory): ')
+    sheet_name = input("Input sheet name (allow to enter empty value if it's first sheet): ")
+    cell_names = list(map(str.strip, input('Input start_cell and finish_cell names, e.g. A1:A10 (select target cells and copy name box): ').split(':')))
 
     # load file, activate sheet in it, select targeted cell's
     wb = load_workbook(filename=filename)
@@ -17,7 +16,9 @@ if __name__ == "__main__":
     cells_with_serial_numbers = ft.create_cells_dictionary(wb_sheet, cell_names)
 
     # change serial numbers for each cell using randomiser, rewrite new serial numbers in file and save it
+    first_symbols = int(input('Сколько первых символов менять? Введи целое положительное число, не превышающее половину длины серийника: ').strip())
+    last_symbols = int(input('Сколько последних символов менять? Введи целое положительное число, не превышающее половину длины серийника: ').strip())
     for key, values in cells_with_serial_numbers.items():
-        wb_sheet[key] = ', '.join([ft.random_last_symbols(value) for value in values])
+        wb_sheet[key] = ', '.join([ft.random_symbols(value, first_symbols, last_symbols) for value in values])
 
     wb.save(f"new_{ft.create_new_filename(filename)}")
